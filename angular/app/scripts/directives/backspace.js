6203f0061adabc5071b7ea4cb57ae484f6cd1bc7
GLips <glipsman@gmail.com>
2013-07-27 22:03:01 -0600

    Add ability to backspace on spaced characters.

--- angular/app/scripts/directives/backspace.js
+++ angular/app/scripts/directives/backspace.js
@@ -4,7 +4,10 @@ myModule.directive('noBackspace', function($document) {
     return function(scope, elm, attrs) {
         $document.keydown(function(evt) {
             key = evt.which
-            if(key == 8)
+
+            if(key == 8 && scope.content.substr(scope.content.length - 1) === ' ')
+                scope.$apply(function(){ scope.content = scope.content.substr(0, scope.content.length - 1); })
+            else if(key == 8)
                 evt.preventDefault()
         });
     }
