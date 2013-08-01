5f32ddd034b5d1d992efa6866ff2163c95f482e7
GLips <glipsman@gmail.com>
2013-07-31 18:44:25 -0600

    Add informative responses on sign in failure and success.

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,4 +1,22 @@
 class Api::SessionsController < Devise::SessionsController
+	respond_to :json
+
+	# GET /resource/sign_in
+	def new
+		self.resource = resource_class.new(sign_in_params)
+		clean_up_passwords(resource)
+		render json: { errors: ['Invalid password or username, check your credentials and try again.'] }
+	end
+
+	# POST /resource/sign_in
+	def create
+		self.resource = warden.authenticate!(auth_options)
+		set_flash_message(:notice, :signed_in) if is_navigational_format?
+		sign_in(resource_name, resource)
+		render json: resource
+	end
+
+
 	def destroy
 		sign_out
 		cookies.delete(:_indelible_session)
@@ -11,6 +29,7 @@ class Api::SessionsController < Devise::SessionsController
 		respond_to do |format|
 
 			format.all { head :no_content }
+			format.any(*navigational_formats) { render json: { logged_out: true } }
 			#render.any(*navigational_formats) {  }
 			#format.any(*navigational_formats) { redirect_to redirect_path }
 		end
