5b78d3db5da73e89b1e8f7bb5e32e2896d9175d2
GLips <glipsman@gmail.com>
2013-08-14 21:20:36 -0600

    Add Bower dependency and initialization to deploy scripts. Update database to use mysql2 adapter as necessary.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,5 @@
+require 'bundler/capistrano'
+
 set :application, 'Indelible'
 set :repository,  'git@github.com:GLips/indelible.git'
 set :deploy_to, '/var/www/sites/indelibleapp.com/html/'
@@ -15,9 +17,12 @@ default_run_options[:pty] = true
 
 depend :remote, :command, 'grunt'
 depend :remote, :command, 'npm'
+depend :remote, :command, 'compass'
+depend :remote, :command, 'bower'
 
 server 'indelibleapp.com', :app, :web, :db, :primary => true
 
+after 'deploy', 'bower:init'
 after 'deploy', 'grunt:init_resources'
 after 'deploy', 'grunt:build'
 after 'deploy', 'deploy:index'
@@ -51,6 +56,21 @@ namespace :grunt do
 	task :build do
 		run "cp -R #{shared_path}/node_modules #{angular_path}"
 		run "cd #{angular_path} && grunt build"
+	end
+
+end
+
+namespace :bower do
+
+	desc 'Install or update frontend packages'
+	task :init do
+		if(!remote_file_exists? "#{angular_path}/app/bower_components")
+			run "cd #{angular_path} && bower install && cp -r app/bower_components #{shared_path}/"
+		else
+			run "cp -r #{shared_path}/bower_components #{angular_path}/app/"
+			run "cd #{angular_path} && bower update"
+		end
+		run "cp -r #{angular_path}/app/bower_components #{shared_path}/"
 	end
 
 end
