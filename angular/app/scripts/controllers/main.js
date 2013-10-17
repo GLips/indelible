86526175dc151cf7a4716badae74f51779ec207c
GLips <glipsman@gmail.com>
2013-10-16 22:18:29 -0600

    Add simple A/B test for main page registration button colors. [Finish #58943250]

--- angular/app/scripts/controllers/main.js
+++ angular/app/scripts/controllers/main.js
@@ -2,7 +2,7 @@
 
 var myModule = angular.module('indelibleApp.controllers');
 
-myModule.controller('MainController', function ($scope, $location, $rootScope, Page, Session, Flash, $analytics) {
+myModule.controller('MainController', function ($scope, $location, $rootScope, Page, Session, Flash, $analytics, Splitter) {
       if(Session.loggedIn)
       {
         Flash.hold();
@@ -10,10 +10,21 @@ myModule.controller('MainController', function ($scope, $location, $rootScope, P
       }
       else
       {
+        var button_test = Splitter.getInstance('register_button_color', ['control', 'bright_red', 'green', 'blue']),
+          result = button_test.check_test();
+        if(result == 'control') {
+          $scope.button_class = 'control-class';
+        } else if(result == 'bright_red') {
+          $scope.button_class = 'bright-red-class';
+        } else if(result == 'green') {
+          $scope.button_class = 'green-class';
+        } else {
+          $scope.button_class = 'blue-class';
+        }
         $scope.page = new Page({new: true});
         $scope.page.clear();
         $scope.page.init();
         $analytics.eventTrack("View landing page");
       }
   });
-myModule.$inject = ['$scope', '$location', '$rootScope', 'Page', 'Session', 'Flash', '$analytics'];
+myModule.$inject = ['$scope', '$location', '$rootScope', 'Page', 'Session', 'Flash', '$analytics', 'Splitter'];
